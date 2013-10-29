824a8fa91fcbb3cc2b13db1b310cad485a4ebaa8
yetianshi <yetianshi@alibaba-inc.com>
2013-10-30 00:00:40 +0800

    选中切换颜色

--- LoveShang/View/LSNavBar.m
+++ LoveShang/View/LSNavBar.m
@@ -12,6 +12,7 @@
 
 }
 @property (nonatomic,strong) UIView *border;
+@property (nonatomic,strong) UIScrollView *sv;
 @end
 
 @implementation LSNavBar
@@ -19,27 +20,30 @@
 
 -(void)viewDidLoad{
     [super viewDidLoad];
-    UIScrollView *sv = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 25, self.view.frame.size.width, 32)];
-    sv.backgroundColor = [UIColor whiteColor];
-    sv.delegate = self;
-    sv.contentSize = CGSizeMake(640, 30);
+    _sv = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 25, self.view.frame.size.width, 32)];
+    _sv.backgroundColor = [UIColor whiteColor];
+    _sv.delegate = self;
+    _sv.contentSize = CGSizeMake(640, 30);
     
-    NSArray *navTitle = @[@"test",@"test",@"test",@"test",@"test"];
+    NSArray *navTitle = @[@"头条",@"活动",@"论坛",@"房产",@"装修"];
     float x = 0;
     for (NSInteger i = 0; i < navTitle.count; i++) {
-        UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(x, 0, 100, 30)];
-        x = x + 100;
+        UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(x, 0, 60, 30)];
+        x = x + 60;
         btn.backgroundColor = [UIColor clearColor];
         btn.tag = 1 << i;
         [btn setTitle:[navTitle objectAtIndex:i] forState:UIControlStateNormal];
-        [btn setTitleColor:[LSColorStyleSheet colorWithName:LSColorNavText] forState:UIControlStateNormal];
+        [btn setTitleColor:[LSColorStyleSheet colorWithName:LSColorBlack] forState:UIControlStateNormal];
+        [btn setTitleColor:[LSColorStyleSheet colorWithName:LSColorNavText] forState:UIControlStateHighlighted];
+        [btn setTitleColor:[LSColorStyleSheet colorWithName:LSColorNavText] forState:UIControlStateSelected];
+        [btn.titleLabel setFont:[UIFont fontWithName:@"Arail" size:16]];
         [btn addTarget:self action:@selector(doAction:) forControlEvents:UIControlEventTouchUpInside];
-        [sv addSubview:btn];
+        [_sv addSubview:btn];
     }
-    _border = [[UIView alloc] initWithFrame:CGRectMake(0, sv.contentSize.height-3 , 100, 3)];
+    _border = [[UIView alloc] initWithFrame:CGRectMake(0, _sv.contentSize.height-3 , 60, 3)];
     _border.backgroundColor = [UIColor redColor];
-    [sv addSubview:_border];
-    [self.view addSubview:sv];
+    [_sv addSubview:_border];
+    [self.view addSubview:_sv];
     //底部分割线
     UIView *bottomLine = [[UIView alloc] initWithFrame:CGRectMake(0, 25+31, self.view.frame.size.width, 1)];
     bottomLine.backgroundColor = [LSColorStyleSheet colorWithName:LSColorGrayLine];
@@ -49,10 +53,17 @@
 }
 
 -(void)doAction:(UIButton *)btn{
+    for (UIButton *sub in [_sv subviews]) {
+        if ([sub isKindOfClass:[UIButton class]]) {
+            sub.selected = NO;
+        }
+    }
+    btn.selected = YES;
     [UIView animateWithDuration:0.3 animations:^{
-        CGRect newFrame = CGRectMake(btn.frame.origin.x, btn.frame.origin.y+30-3, 100, 3);
+        CGRect newFrame = CGRectMake(btn.frame.origin.x, btn.frame.origin.y+30-3, 60, 3);
         _border.frame = newFrame;
     }];
+    
     if ([_delegate respondsToSelector:@selector(doActionWithBtn:)]) {
         [_delegate doActionWithBtn:btn];
     }
